10b77613f13e224bd71c8d4b72d8734596049986
LedgeAtSchool <128752437+LedgeAtSchool@users.noreply.github.com>
2023-04-12 15:49:38 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,20 +1,23 @@
 function furryize() {
-        // Get the input box element and the furrytext element
-        const inputBox = document.getElementById("input-box");
-        const furryText = document.getElementById("furrytext");
+  // Get the input box element and the furrytext element
+  const inputBox = document.getElementById("input-box");
+  const furryText = document.getElementById("furrytext");
 
-        // Replace all "r" and "l" characters with "w"
-        let text = inputBox.value.replace(/[rl]/gi, "w");
+  // Replace all "r" and "l" characters with "w"
+  let text = inputBox.value.replace(/[rl]/gi, "w");
 
-        // Replace all "." characters with " uwu~"
-        text = text.replace(/\./g, " uwu~");
+  // Replace all "." characters with "~"
+  text = text.replace(/\./g, "~");
 
-        // Replace ":)" with ":3"
-        text = text.replace(/:\)/g, ":3");
+  // Replace ":)" with ":3"
+  text = text.replace(/:\)/g, ":3");
 
-        // Add "owo" before each question mark
-        text = text.replace(/\?/g, " owo?");
+  // Add "owo" before each question mark
+  text = text.replace(/\?/g, " owo?");
 
-        // Set the text of the furrytext element to the furryfied text
-        furryText.textContent = text;
-      }
+  // Add " nwn" after each exclamation mark
+  text = text.replace(/!/g, " nwn!");
+
+  // Set the text of the furrytext element to the furryfied text
+  furryText.textContent = text;
+}
