211bb4dc9d068e3caf1221d7d9628f3b66066634
LedgeAtSchool <128752437+LedgeAtSchool@users.noreply.github.com>
2023-04-12 16:17:55 -0700

    Update main.js

--- main.js
+++ main.js
@@ -3,8 +3,15 @@ function furryize() {
   const inputBox = document.getElementById("input-box");
   const furryText = document.getElementById("furrytext");
 
-  // Replace all "r" and "l" characters with "w"
-  let text = inputBox.value.replace(/[rl]/gi, (match) => match === 'r' ? 'w' : 'l' ? 'W' : 'w');
+  // Replace "r" with "w" and "l" with "W" only if the original character is lowercase,
+  // and replace "r" with "w" and "l" with "w" only if the original character is uppercase
+  let text = inputBox.value.replace(/[rl]/g, (match) => {
+    if (match === 'r') {
+      return inputBox.value.match(/[RL]/) ? 'R' : 'w';
+    } else if (match === 'l') {
+      return inputBox.value.match(/[RL]/) ? 'L' : 'W';
+    }
+  });
 
   // Replace all "." characters with "~"
   text = text.replace(/\./g, "~");
